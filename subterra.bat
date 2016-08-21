9fd33e4c8e673e43203748075dd0b78572f548ca
tripl3dogdare <connor@connorjulian.org>
2016-08-20 22:01:01 -0600

    Revert subterra.bat
    Update caused issues with user input

--- subterra.bat
+++ subterra.bat
@@ -1,8 +1,2 @@
 @echo off
-IF [%JUSTTERMINATE%] == [OKAY] (
-    SET JUSTTERMINATE=
-		python "%~dp0/bin/subterra.py" %*
-) ELSE (
-    SET JUSTTERMINATE=OKAY
-    CALL %0 %* <NUL
-)
+"%~dp0/bin/subterra.py" %*
